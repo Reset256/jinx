95350373cfe6735e17e93786e9b0e6b202849bba
Victor Suchilin <suchilin2007@yahoo.com>
2022-11-03 00:19:57 +0400

    FolderWatcherService refactored to single thread

--- core/src/main/java/org/java/indexer/core/index/FolderWatcherService.java
+++ core/src/main/java/org/java/indexer/core/index/FolderWatcherService.java
@@ -8,28 +8,22 @@ import java.nio.file.Path;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 public class FolderWatcherService {
 
-    private final ExecutorService executorService;
     private final Set<Path> watchedFolder;
     private final FolderWatcher folderWatcher;
 
     public FolderWatcherService(List<String> ignoredNames, Index index) {
-        executorService = Executors.newFixedThreadPool(1, r -> {
-            Thread t = Executors.defaultThreadFactory().newThread(r);
-            t.setDaemon(true);
-            return t;
-        });
         this.watchedFolder = new HashSet<>();
         try {
             folderWatcher = new FolderWatcher(FileSystems.getDefault().newWatchService(), index, ignoredNames);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        executorService.submit(folderWatcher);
+        final Thread thread = new Thread(folderWatcher);
+        thread.setDaemon(true);
+        thread.start();
     }
 
     public void watchFolders(Path folderPath) {
